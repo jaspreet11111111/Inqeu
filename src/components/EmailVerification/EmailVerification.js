f8d68ab3cc4e00d5f3b1b384ada9611bb2d1138c
Ansh Jhanwar <maheshwariansh720@gmail.com>
2023-02-09 02:28:11 +0530

    Fixes ui of otp screen

--- src/components/EmailVerification/EmailVerification.js
+++ src/components/EmailVerification/EmailVerification.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import { generateOtp } from '../../actions/otp'
 import { USER_OTP_SUCCESS } from '../../constants/actionType';
+import Contact from '../elements/Contact/Contact';
+import Getapp from '../elements/GetApp/Getapp';
 
 const EmailVerification = () => {
 	const userInfo = useSelector(state => state.userRegister);
@@ -22,47 +24,53 @@ const EmailVerification = () => {
 		dispatch(generateOtp({ ...formData, userId: userInfo.userInfo.user._id, some: 'some', loda: 'jaspreet' }, navigate))
 	}
 	return (
-		<Card sx={{
-			width: {
-				xs: '95%',
-				sm: '95%',
-				md: '50%',
-				lg: '40%'
-			},
-			margin: 'auto',
-			padding: '2em',
-			marginTop: '2em',
-			marginBottom: '2em'
-		}}>
-			<Typography fontSize='24px'>Please verify OTP</Typography>
-			<form onSubmit={handleSub}>
-				<Stack
-					bgcolor="#ffff"
-					direction="row"
-					border="1px solid rgba(0, 0, 0, 0.12)"
-					sx={{
-						width: '100%',
-						height: '48px',
-						borderRadius: '10px',
-						marginBottom: '24px'
-					}}>
-					{/* <PersonIcon sx={{
+		<>
+
+
+			<Card sx={{
+				width: {
+					xs: '95%',
+					sm: '95%',
+					md: '50%',
+					lg: '40%'
+				},
+				margin: 'auto',
+				padding: '2em',
+				marginTop: '2em',
+				marginBottom: '2em'
+			}}>
+				<Typography fontSize='24px'>Please verify OTP</Typography>
+				<form onSubmit={handleSub}>
+					<Stack
+						bgcolor="#ffff"
+						direction="row"
+						border="1px solid rgba(0, 0, 0, 0.12)"
+						sx={{
+							width: '100%',
+							height: '48px',
+							borderRadius: '10px',
+							marginBottom: '24px'
+						}}>
+						{/* <PersonIcon sx={{
 						color: '#880ED4',
 						padding: '12px 20px'
 					}} /> */}
-					<InputBase
-						placeholder='Username'
-						name='username'
-						onChange={e => setFormData({ ...formData, otp: e.target.value })}
-						autoFocus={true}
-						fullWidth
-						required={true}
-						aria-label='Username'
-					/>
-				</Stack>
-				<Button type='submit'>Submit</Button>
-			</form>
-		</Card>
+						<InputBase
+							placeholder='Username'
+							name='username'
+							onChange={e => setFormData({ ...formData, otp: e.target.value })}
+							autoFocus={true}
+							fullWidth
+							required={true}
+							aria-label='Username'
+						/>
+					</Stack>
+					<Button type='submit'>Submit</Button>
+				</form>
+			</Card>
+			<Getapp />
+			<Contact />
+		</>
 	)
 }
 
